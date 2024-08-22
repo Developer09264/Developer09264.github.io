6914d5ec3ae138caec77b522e161f9a863a465dd
Developer09264 <userc09264@gmail.com>
2024-08-22 22:45:13 +0800

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,22 @@ let quotes = [];
 
 // 显示随机田语
 function showRandomQuote() {
-    if (quotes.length === 0) return; // 如果quotes为空，直接返回
+  if (quotes.length === 0) return; // 如果quotes为空，直接返回
 
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const newQuote = quotes[randomIndex];
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const newQuote = quotes[randomIndex];
 
-    // 添加淡出效果
-    quoteElement.style.opacity = 0;
+  // 添加淡出效果
+  quoteElement.style.opacity = 0;
 
-    // 设置新的田语内容
+  // 设置新的田语内容
+  setTimeout(() => {
+    quoteElement.textContent = newQuote;
+    // 添加淡入效果
     setTimeout(() => {
-        quoteElement.textContent = newQuote;
-        // 添加淡入效果
-        setTimeout(() => {
-            quoteElement.style.opacity = 1;
-        }, 100); // 在0.1秒后淡入
-    }, 500); // 在0.5秒后改变内容
+      quoteElement.style.opacity = 1;
+    }, 100); // 在0.1秒后淡入
+  }, 500); // 在0.5秒后改变内容
 }
 
 // 异步加载田语数据（假设quotes.txt包含每行一个田语）
@@ -31,10 +31,10 @@ fetch("assets/quotes.txt")
   })
   .then(data => {
     quotes = data.split('\n').filter(quote => quote.trim() !== '');
-    
+
     // 在页面加载时显示随机田语
     showRandomQuote();
-    
+
     // 将所有田语添加到田语列表
     quotes.forEach(quote => {
       const listItem = document.createElement("li");
@@ -52,25 +52,33 @@ setInterval(showRandomQuote, 3000);
 
 //检查屏幕滚动
 function checkScroll() {
-    const scrollPosition = window.scrollY || window.pageYOffset;
-    const backToTopButton = document.getElementById('back-to-top');
+  const scrollPosition = window.scrollY || window.pageYOffset;
+  const backToTopButton = document.getElementById('back-to-top');
 
-    if (!backToTopButton) {
-        console.error("Element with id 'back-to-top' not found");
-        return;
-    }
-    
-    //判断位置，为按钮元素添加类
-    if (scrollPosition > window.innerHeight / 2) {
-        backToTopButton.classList.add('show');
-    } else {
-        backToTopButton.classList.remove('show');
-    }
+  if (!backToTopButton) {
+    console.error("Element with id 'back-to-top' not found");
+    return;
+  }
+
+  //判断位置，为按钮元素添加类
+  if (scrollPosition > window.innerHeight / 2) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
 }
 
 window.addEventListener('scroll', checkScroll);
 window.addEventListener('load', checkScroll);
 
-document.getElementById('back-to-top')?.addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+document.getElementById('back-to-top')?.addEventListener('click', function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.addEventListener('visibilitychange', function () {
+  if (document.visibilityState === 'hidden') {
+    document.title = '人在做，天在看';
+  } else {
+    document.title = '田语 | 朴实无华，意蕴悠长';
+  }
 });
